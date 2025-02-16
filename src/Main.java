1ba3e398557d0eb8a17e2d7cd525056667a94c4f
Evan <spanglem@uc.edu>
2025-02-16 18:13:54 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double itemCost= 0;
+        double shipCost= 0;
+        double totalCost= 0;
+        double totalShipCost= 0;
+        final double SHIP_RATE= .02;
+        String trash = "";
+
+        System.out.print("Please enter the price of your item in USD: ");
+        if (in.hasNextDouble())
+        {
+            itemCost=in.nextDouble();
+            in.nextLine();
+            if(itemCost < 100)
+            {
+                shipCost = itemCost * SHIP_RATE;
+            }
+            totalCost = itemCost + shipCost;
+            System.out.println("Your shipping cost is $" + shipCost);
+            System.out.println("Your total cost is $" + totalCost);
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("Invalid Item Price: " + trash);
+            System.exit(0);
+        }
+
+    }
+}
